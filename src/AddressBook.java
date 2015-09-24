85b441683cb52ac76624f91c3dbddc4695093813
vanjaveselinovic <vanjaveselinovic@5010-028.ODENG2.CARLETON.CA>
2015-09-24 12:03:07 -0400

    Setting up

---
+++ src/AddressBook.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+	ArrayList<BuddyInfo> buddies = new ArrayList<BuddyInfo>();
+	public void addBuddy(BuddyInfo buddy){
+		buddies.add(buddy);
+	}
+	public void removeBuddy(BuddyInfo buddy){
+		buddies.remove(buddy);
+	}
+	public static void main(String[] args){
+		System.out.println("Address book");
+		AddressBook addressBook = new AddressBook();
+		BuddyInfo buddy1 = new BuddyInfo();
+		addressBook.addBuddy(buddy1);
+		addressBook.removeBuddy(buddy1);
+	}
+}
